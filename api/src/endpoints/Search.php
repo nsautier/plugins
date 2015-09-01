f2f8ddc3b422c7ee95e52130a343f08111d98887
Nelson Zamith <nzamith@teclib.com>
2015-09-01 16:28:37 +0200

    modified usage of needsScope in Search endpoint

--- api/src/endpoints/Search.php
+++ api/src/endpoints/Search.php
@@ -10,35 +10,38 @@
 
 use \API\Core\Tool;
 use \Illuminate\Database\Capsule\Manager as DB;
+use \API\OAuthServer\OAuthHelper;
 
 // Minimal length of search string
 $search_min_length = 2;
 
 $search = function() use($app) {
+   OAuthHelper::needsScopes(['plugins:search']);
+
    global $search_min_length,
-          $allowed_languages;
+         $allowed_languages;
 
    $body = Tool::getBody();
    if ($body == NULL ||
-       !isset($body->query_string) ||
-       strlen($body->query_string) < $search_min_length ) {
+      !isset($body->query_string) ||
+      strlen($body->query_string) < $search_min_length ) {
 
-      return Tool::endWithJson([
-         "error" => "Your search string needs to ".
-                  "have at least ".$search_min_length." chars"
-      ], 400);
+     return Tool::endWithJson([
+        "error" => "Your search string needs to ".
+                 "have at least ".$search_min_length." chars"
+     ], 400);
    }
    $query_string = $body->query_string;
 
    $_search = Tool::paginateCollection(
-                    \API\Model\Plugin::short()
-                               ->with('authors', 'versions', 'descriptions')
-                               ->withDownloads()
-                               ->withAverageNote()
-                               ->descWithLang(Tool::getRequestLang())
-                         ->where('name', 'LIKE', "%$query_string%")
-                         ->orWhere('plugin_description.short_description', 'LIKE', "%$query_string%")
-                         ->orWhere('plugin_description.long_description', 'LIKE', "%$query_string%"));
+                   \API\Model\Plugin::short()
+                              ->with('authors', 'versions', 'descriptions')
+                              ->withDownloads()
+                              ->withAverageNote()
+                              ->descWithLang(Tool::getRequestLang())
+                        ->where('name', 'LIKE', "%$query_string%")
+                        ->orWhere('plugin_description.short_description', 'LIKE', "%$query_string%")
+                        ->orWhere('plugin_description.long_description', 'LIKE', "%$query_string%"));
    Tool::endWithJson($_search);
 };
 
